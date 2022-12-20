8415463636c2bebf3566d0fc1851297c985c11af
Alexandra2888 <moldovan.alexandra28@gmail.com>
2022-12-20 23:05:14 +0200

    adding project

---
+++ app.js
@@ -0,0 +1,16 @@
+const jokeBtn = document.querySelector("button");
+const jokePara = document.querySelector(".joke");
+
+jokeBtn.addEventListener("click", getJoke);
+
+async function getJoke() {
+    const jokeUrl = 'https://icanhazdadjoke.com/';
+    const response = await fetch(jokeUrl, {
+        headers: {
+            Accept: "application/json",
+        },
+    });
+
+    const responseData = await response.json();
+    jokePara.innerText = responseData.joke;
+}
